4da896f04744fac9a6ef3b8fa329df5b91444068
henrymedeiros <henrymedeiros77@gmail.com>
2021-08-10 15:22:24 -0300

    Configurando

--- textFormatter.py
+++ textFormatter.py
@@ -20,23 +20,29 @@ class TelaPrincipal:
     def __init__(self):
         #Layout
         layout = [
-            [sg.Text('Texto')],
-            [sg.Multiline(size=(50,6), key='entrada')],
-            [sg.Text('Substituir caracteres')],
-            [sg.Text('Antigo'),sg.Input(key='old_char'), sg.Text('Novo'), sg.Input(key='new_char')],
+            [sg.Text('Seu texto')],
+            [sg.Multiline(size=(50,6), key='input')],
+            [sg.Text('Escolha uma opção abaixo')],
+            [sg.Radio('Substituir', 'options', key='replace')],
+            [sg.Text('Antigo termo'), sg.Input(size=(5,0), key='old_char'), sg.Text('Novo termo'), sg.Input(size=(5,0), key='new_char')],
+            [sg.Radio('Inserir no fim', 'options', key='insert_at_end')],
             [sg.Button('Processar')]
+            
             
         ]
         #Janela
-        self.janela = sg.Window('Stringnator').layout(layout)
+        self.janela = sg.Window('Super Text Formatter').layout(layout)
         self.button, self.values = self.janela.Read()
         #Extrair
         
     def Iniciar(self):
-        
-        entrada = self.values['entrada']
+        user_input = self.values['input']
+        replace = self.values['replace']
         old_char = self.values['old_char']
         new_char = self.values['new_char']
+        insert_at_end = self.values['insert_at_end']
+        print(self.values)
+
         
 tela = TelaPrincipal()
 tela.Iniciar()
